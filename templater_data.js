fa640c4acf6868bd57767c0227019b8f1eb85dda
Denis Dumulong <dumulong@sbcglobal.net>
2025-07-02 21:58:05 -0500

    first version

---
+++ templater_data.js
@@ -0,0 +1,15 @@
+const data = {
+    "test" : {
+        what : "Example for the test1 link, when prompted, enter 'feed/history'",
+        "test1" : "https://www.youtube.com/[what]"
+    },
+    "test2" : {
+        "level" : "first level",
+        "another level" : {
+            hidden : {
+                tree : "of life"
+            },
+            "my thing" : "Is the tree {tree}"
+        }
+    }
+}
