ff048293cfd8822d111f37f5fdfd89b9cffcebed
Denis Dumulong <dumulong@sbcglobal.net>
2025-07-05 20:41:17 -0500

    Modified

--- templater_data.js
+++ templater_data.js
@@ -1,4 +1,4 @@
-const data = {
+templaterData["Test Data"] = {
     "test" : {
         what : "Example for the test1 link, when prompted, enter 'feed/history'",
         "test1" : "https://www.youtube.com/[what]"
---
+++ templater_data2.js
@@ -0,0 +1,6 @@
+templaterData["Other data"] = {
+    "test again" : {
+        what : "Example for the test1 link, when prompted, enter 'feed/history'",
+        "test1" : "https://www.youtube.com/[what]"
+    }
+}
